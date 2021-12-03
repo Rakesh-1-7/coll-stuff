4530b6afafbeeabcbac9c38398c4a6b39e88aba4
Rakesh B N <68104336+Rakesh-1-7@users.noreply.github.com>
2021-12-03 07:41:13 +0530

    Another algorithm for word reversing

---
+++ My File.py
@@ -0,0 +1,17 @@
+print("This is word reversing algorithm")
+def Name_call():
+	sentence = input("Please enter the word to be reversed : ")
+	word_counts = { }
+
+	word_counts = sentence.split(' ')
+
+	for x in range(len(word_counts)):
+  		word_counts[x] = (word_counts[x])[::-1]
+
+	full_sentence = " ".join(word_counts)
+	print(full_sentence)
+	Name_call()
+Name_call() 
+
+
+
